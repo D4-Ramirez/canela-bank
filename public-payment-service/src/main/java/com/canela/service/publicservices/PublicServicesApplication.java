a3d6355a9fe9e13b5552187273a1625bde3fb24d
David Ramirez <ramirezmonroyd@gmail.com>
2023-02-13 13:06:44 -0500

    style: 🚚 rename public service folder

---
+++ public-payment-service/src/main/java/com/canela/service/publicservices/PublicServicesApplication.java
@@ -0,0 +1,13 @@
+package com.canela.service.publicservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicServicesApplication.class, args);
+	}
+
+}
